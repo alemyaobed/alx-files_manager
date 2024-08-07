200ca3b4cfd35dc99529746f6f3df695484ee4cd
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-08-07 14:15:51 +0000

    some minor changes

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 import express from 'express';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import cors from 'cors';
 import routes from './routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 // Load routes
