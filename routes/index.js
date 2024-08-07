41409525bd76d6b3efaa41f8a07689fd7bbae20a
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-08-07 15:38:37 +0000

    the implementation of the users controller and route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 // The routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// New user route
+router.post('/users', UsersController.postNew);
 
 export default router;
